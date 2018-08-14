e866abfcb80268dbc7ca7fa26a4c29e43e968fc9
AjinkyaPrabhu <33144045+AjinkyaPrabhu@users.noreply.github.com>
2018-08-14 22:53:43 +0530

    Add files via upload
    Fixed Algorithm for RL and added saving and restoring feature for model and logging

--- gyms_test.py
+++ gyms_test.py
@@ -3,15 +3,17 @@ import numpy as np
 import time
 import tensorflow as tf
 from reinforcement_learning import Deep_model
-
+import os
 
 model = Deep_model()
 model.create_model()
 model.setup_train_model()
 
-
+tf.set_random_seed(1)
 env = gym.make("SpaceInvaders-v0")
 env.reset()
+
+saver = tf.train.Saver()
 
 NUM_EPISODES = 20000
 
@@ -26,18 +28,7 @@ print("POSSIBLE ACIONS ARE:",env.action_space)
    3 : "RIGHT",
    4 : "LEFT",
    5 : "DOWN",
-   6 : "UPRIGHT",
-   7 : "UPLEFT",
-   8 : "DOWNRIGHT",
-   9 : "DOWNLEFT",
-   10 : "UPFIRE",
-   11 : "RIGHTFIRE",
-   12 : "LEFTFIRE",
-   13 : "DOWNFIRE",
-   14 : "UPRIGHTFIRE",
-   15 : "UPLEFTFIRE",
-   16 : "DOWNRIGHTFIRE",
-   17 : "DOWNLEFTFIRE",
+
 
 """
 
@@ -49,11 +40,9 @@ def countDown(x):
 
 
 
-countDown(4)
+countDown(1)
 
-reward_data = []
-observation_data = []
-action_data = []
+
 
 
 def to_one_hot(index):
@@ -61,32 +50,88 @@ def to_one_hot(index):
     x[index] = 1
     return x
 
+
+def create_new_reward(reward):
+    return reward-1000
+
+
+def finish_episode():
+    pass
+
+
+
+
 with tf.Session() as sess:
+
+    print("Trying to restore saved model")
+    if  os.path.isdir("./saved_models/"):
+        saver.restore(sess,'./saved_models/model.ckpt')
+        print("Model Restored")
+    else:
+        print("No Saved Model found....Starting Fresh")
+
     sess.run(tf.global_variables_initializer())
     for episode in range(NUM_EPISODES):
+        l1 = 3
         observation  = env.reset()
-
+        reward_data = []
+        observation_data = []
+        action_data = []
         time_steps = 0
-        while True:
+        while time_steps<10000:
             time_steps+=1
             env.render()
-
+            print(time_steps)
             observation = observation.reshape((1,210,160,3))
+
             action_sampled = sess.run(model.sample_op,feed_dict={model.observation_placeholder:observation})
+            action = action_sampled[0,0]
+            #dropout type
 
 
-            observation,reward,done,info = env.step(action_sampled[0,0])
+            observation,reward,done,info = env.step(action)
+
+
+
+
+
+            # l2 = info['ale.lives']
+            # if l2<l1:
+            #     l1 = l2
+            #     reward = create_new_reward(reward)
+
+
 
             reward_data.append(reward)
             action_data.append(to_one_hot(action_sampled[0,0]))
             observation_data.append(observation)
 
-            print(reward)
+
+
+
+
             if done:
-                print(reward_data)
-                print(action_data)
-                print(observation_data)
-                sess.run(model.train_op,feed_dict={model.observation_placeholder:np.array(observation_data),
-                model.action_placeholder:np.array(action_data),model.rewards_placeholder:np.array(reward_data)})
+
+                action_data = np.array(action_data)
+                observation_data = np.array(observation_data)
+                reward_data = np.array(reward_data) #sum(reward_data)* np.ones((action_data.shape[0])) #
+                final_rewards = []
+                R = 0
+                for r  in reward_data[::-1]:
+                    R = r + 0.7 * R
+                    final_rewards.insert(0,R)
+
+                final_rewards = np.array(final_rewards)
+
+                final_rewards = (final_rewards-final_rewards.mean())/(final_rewards.std() + 1e-4)
+                print(final_rewards)
+                print(final_rewards.shape)
+                print(action_data.shape)
+                print(observation_data.shape)
+                model.train_model_batches(sess,final_rewards,action_data,observation_data,episode=episode)
+                # model.write_graph_and_summary(sess,action_data,observation_data,episode)
                 print("Episode done after {} timesteps",time_steps)
+                print("Saving model.....")
+                saver.save(sess,'./saved_models/model.ckpt')
+
                 break
--- reinforcement_learning.py
+++ reinforcement_learning.py
@@ -1,20 +1,21 @@
 
 import tensorflow as tf
+
+
 class Deep_model:
 
 
     def create_model(self,n_classes=6):
 
-
-
         self.observation_placeholder = tf.placeholder(dtype='float',shape = [None,210,160,3])
-        self.rewards_placeholder = tf.placeholder(dtype = float , shape =[None])
+        self.rewards_placeholder = tf.placeholder(dtype = 'float' , shape =[None])
         self.action_placeholder = tf.placeholder(dtype='float',shape=[None,n_classes])
 
-
+        self.avg_reward = tf.reduce_mean(self.rewards_placeholder)
         self.layer1 = tf.layers.conv2d(self.observation_placeholder,filters = 64,kernel_size = [5,5],padding="same",activation=tf.nn.relu)
         self.layer1_flat = tf.layers.flatten(self.layer1)
-        self.prediction = tf.layers.dense(self.layer1_flat,n_classes)
+        self.layer2 = tf.layers.dense(self.layer1_flat,activation=tf.nn.relu,units =30)
+        self.prediction = tf.layers.dense(self.layer2,n_classes)
 
         self.opt = tf.train.AdamOptimizer()
         self.sample_op = tf.multinomial(logits = self.prediction,num_samples=1)
@@ -23,26 +24,51 @@ class Deep_model:
     def setup_train_model(self):
         ##change loss
         self.loss = tf.reduce_sum(self.rewards_placeholder*tf.nn.softmax_cross_entropy_with_logits(logits = self.prediction,labels = self.action_placeholder))
+        self.writer = tf.summary.FileWriter('./logs')
+
+        tf.summary.scalar("TOTAL LOSS",self.avg_reward)
+        self.merged_summary = tf.summary.merge_all()
+
+
         self.train_op = self.opt.minimize(loss=self.loss)
 
 
-    # def write_graph_and_summary(self,sess,action_data,observation_data):
-    #     writer = tf.summary.FileWriter('./logs')
-    #     writer.add_graph(graph=sess.graph)
-    #     tf.summay.scalar("TOTAL LOSS",self.loss)
-    #     merged_summary = tf.summary.merge_all()
+    # def write_graph_and_summary(self,sess,action_data,observation_data,episode):
     #
-    #     sess.run(merged_summary,feed_dict={self.observation_placeholder=observation_data,self.action_placeholder:action_data})
+    #     s = sess.run(self.merged_summary,feed_dict={self.observation_placeholder:observation_data,
+    #                                             self.action_placeholder:action_data})
+    #     self.writer.add_summary(s,episode)
     #
 
+    def train_model_batches(self,sess,reward_data,action_data,observation_data,episode,batch_size=20):
+
+        m = action_data.shape[0]
+        i = 0
+        x = i * batch_size
+
+        for i in range(m//batch_size):
+            x = i * batch_size
+            sess.run(self.train_op,feed_dict={self.observation_placeholder:observation_data[x:x+batch_size,:],
+                                               self.action_placeholder:action_data[x:x+batch_size,:],self.rewards_placeholder:reward_data[x:x+batch_size]})
+            print("Batch:",i,"trained")
+        print(x)
+        _,s = sess.run([self.train_op,self.merged_summary],feed_dict={self.observation_placeholder:observation_data[x:,:],
+                                          self.action_placeholder:action_data[x:,:],self.rewards_placeholder:reward_data[x:]})
+        self.writer.add_summary(s,episode)
 
 
 
 
 
-if __name__ == '__main__':
-    x = Deep_model()
-    x.create_model()
 
-    print(x.prediction)
-    x.setup_train_model()
+
+#
+# if __name__ == '__main__':
+#     x = Deep_model()
+#     x.create_model()
+#
+#
+#
+#
+#     print(x.prediction)
+#     x.setup_train_model()
